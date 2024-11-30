43dc0746930eb449fc5e9ba012240e740fc15337
keven <kevenalisson@gmail.com>
2024-11-30 10:29:19 -0300

    feat: layout nao estava atualizando otitulo e a descrição da landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Una - Educação Ambiental e Gamificação Integrada',
   description: 'Transforme sua comunidade através de práticas sustentáveis',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
+      <head />
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
